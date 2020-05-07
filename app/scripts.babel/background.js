578c3587e84354c5445a3525d043d6494058738f
andreiashu <andreiashu@gmail.com>
2020-05-07 16:51:30 +0700

    match by url; update popup, badge;

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,9 +1,25 @@
-'use strict';
+'use strict'
 
 chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
-});
+  console.log('onInstalled', details)
+  chrome.storage.local.set({ debug: '*' })
+  console.log('previousVersion', details.previousVersion)
+})
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.browserAction.setBadgeBackgroundColor({ color: '#828282' })
+chrome.browserAction.setBadgeText({ text: 'mtn' })
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+// console.log('\'Allo \'Allo! Event Page for Browser Action')
+
+chrome.runtime.onMessage.addListener(function (req, sender) {
+  console.log('onmessage', req)
+  if (req.matches) {
+    chrome.browserAction.setBadgeBackgroundColor({ color: '#02a61d' })
+    chrome.browserAction.setBadgeText({ text: 'meme' })
+
+    console.log('[background script] got matches: req', req)
+    chrome.storage.local.set({ matches: req.matches })
+    chrome.pageAction.show(sender.tab.id)
+    chrome.pageAction.setTitle({ tabId: sender.tab.id, title: req.matches[0].article_title })
+  }
+})
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,27 @@
-'use strict';
+'use strict'
 
-console.log('\'Allo \'Allo! Popup');
+const showMatches = () => {
+  chrome.storage.local.get(['matches'], (matches) => {
+    updateDom(matches.matches)
+  })
+}
+
+const updateDom = (matches) => {
+  console.log('POPUP got matches', matches)
+  const wrapper = document.getElementById('matches')
+  for (const match of matches) {
+    const card = createRedditCard(match)
+    wrapper.appendChild(card)
+  }
+}
+
+const createRedditCard = item => {
+  const bqElem = document.createElement('blockquote')
+  bqElem.className = 'reddit-card'
+
+  bqElem.innerHTML = `<a href="${item.url}">${item.title}</a> from <a href="${item.sub_url}">${item.sub_name}</a>`
+
+  return bqElem
+}
+
+window.onload = showMatches
